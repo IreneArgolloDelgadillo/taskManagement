1519a591069d3b5dfcce806b4066155556eba653
IreneArgollo <ireneirene892@gmail.com>
2016-11-11 00:11:40 -0400

    task management first commit

---
+++ Main.js
@@ -0,0 +1,65 @@
+﻿function loadJSON(path, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    var content = JSON.parse(xhr.responseText)
+                success(content);
+                return content;
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
+
+var taskPath = '../Data/tasks.json';
+var answerablePath = '../Data/answerables.json';
+
+
+loadJSON(taskPath, 
+        function (data) { 
+          chargeTasks(data);
+        },
+        function (xhr) { console.error(xhr); }
+);
+
+loadJSON(answerablePath, 
+        function (data) { 
+          chargeAnswerables(data);
+        },
+        function (xhr) { console.error(xhr); }
+);
+
+function chargeTasks(data){
+  var tasks = data['tasks'];
+  var content ="";
+  for (var i = tasks.length - 1; i >= 0; i--) {
+    content = content + 
+      "<li class=\"collection-item avatar\">"+
+        "<i class=\"material-icons circle green\">assignment</i>"+
+        "<span class=\"title\">"+tasks[i]['title']+"</span>"+
+        "<p>Fecha Limite "+tasks[i]['deadLine']+"<br>"+
+        "Postdata "+tasks[i]['description']+"</p>"+
+      "</li>";
+  } 
+  document.getElementById("taskList").innerHTML += content;
+}
+
+function chargeAnswerables(data){
+  var answerables = data['answerables'];
+  var content ="";
+  for (var i = answerables.length - 1; i >= 0; i--) {
+    content = content + 
+    "<li class=\"collection-item avatar\">"+
+        "<img src=\"" + answerables[i]['photoPath'] + "\" class=\"circle\">"+
+        "<span class=\"title\">"+answerables[i]['fullName']+"</span>"+
+        "<p>Especialty "+answerables[i]['especialty'] +"</p>"+
+      "</li>";
+  } 
+  document.getElementById("answerableList").innerHTML += content;
+}
---
+++ Scripts/main.js
@@ -0,0 +1,34 @@
+﻿document.querySelector('h1').onclick = function () {
+    alert(':) X');
+}
+
+var imgOscuro = 'Images/oscuro.png';
+var imgLuz = 'Images/luz.jpg';
+
+var miImage = document.querySelector('img');
+
+miImage.onclick = function () {
+    var miSrc = miImage.getAttribute('src');
+    if (miSrc === imgOscuro) {
+        miImage.setAttribute('src', imgLuz);
+    } else {
+        miImage.setAttribute('src', imgOscuro);
+    }
+}
+
+var miBoton = document.querySelector('button');
+var miTitulo = document.querySelector('h1');
+
+function seleccionnombre() {
+    var miNombre = prompt('por favor ingresa tu nombre.');
+    localStorage.setItem('nombre', miNombre);
+    miTitulo.innerHTML = 'Hola , ' + miNombre;
+}
+
+task = {
+    title: "Read a orm's book",
+    pageQuantity: 12,
+    athor: { name: "juan", lastName: "pere" }
+}
+
+console.log(task.title);
